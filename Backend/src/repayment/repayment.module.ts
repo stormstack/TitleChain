0f098acd6e064a22e6b08ec878ec135ca79d7a81
nafiuishaaq <nafiuig@gmail.com>
2026-05-11 10:22:06 +0100

    implemented the repayment

--- Backend/src/repayment/repayment.module.ts
+++ Backend/src/repayment/repayment.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RepaymentService } from './repayment.service';
 import { RepaymentSplitter } from './repayment-splitter';
